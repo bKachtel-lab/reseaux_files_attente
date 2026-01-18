e7111dc799c0d301b2a22c2cb9207efd77ef4a97
Boussad HAMMOUM <boussad.hammoum@etu.univ-lehavre.fr>
2026-01-18 15:49:41 +0100

    Creation de la classe <Client>

---
+++ src/main/java/mis/RFA/Client.java
@@ -0,0 +1,35 @@
+package mis.RFA;
+
+public class Client {
+	private final int id;
+	
+	private final double instantArrivee;
+	private double instantSortie;
+	
+	public Client(int id, double instantArriveeSysteme) {
+		this.id = id;
+		this.instantArrivee = instantArriveeSysteme;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public double getInstantArrivee() {
+		return instantArrivee;
+	}
+	
+	public double getInstantSortie() {
+		return instantSortie;
+	}
+	
+	public void setInstantSortie(double t) {
+		this.instantSortie = t;
+	}
+	
+	public double getTempsPresences() {
+		return instantSortie - instantArrivee;
+	}
+	
+	
+}
