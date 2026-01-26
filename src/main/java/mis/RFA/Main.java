30bfb57ffa7029ead344ecb6951106eec78a1f19
Boussad Hammoum <boussad.hammoum@etu.univ-lehavre.fr>
2026-01-26 16:28:17 +0100

    Correction des unités valeurs dans le main

--- src/main/java/mis/RFA/Main.java
+++ src/main/java/mis/RFA/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         // Paramètres communs
     	
-        double muC = 100.0;          // Coordinateur : 10 ms
+        double muC = 1/10.0;          // Coordinateur : 10 ms
         double p = 0.5;              // Probabilité de sortie
         double dureeSimulation = 30000.0;
 
@@ -15,7 +15,7 @@ public class Main {
 
         double lambda1 = 0.002;
 
-        double[] muServeurs1 = { 0.0083 };   // 120 ms
+        double[] muServeurs1 = { 1/120.0 };   // 120 ms
         double[] q1 = { 0.5 };
 
         ReseauFilesAttente rfa1 = new ReseauFilesAttente(
@@ -33,7 +33,7 @@ public class Main {
         // TEST 2 : 1 serveur rapide + 1 lent
         System.out.println("\n= TEST 2 : 1 rapide + 1 lent =");
 
-        double[] muServeurs2 = { 0.00833, 0.00417 };  // 120 ms et 240 ms
+        double[] muServeurs2 = { 1/120.0, 1/120.0 };  // 120 ms et 240 ms
         double[] q2 = { 0.25, 0.25 };
 
         ReseauFilesAttente rfa2 = new ReseauFilesAttente(
@@ -50,7 +50,7 @@ public class Main {
         // TEST 3 : 1 serveur rapide + 2 serveurs lents
         System.out.println("\n=== TEST 3 : 1 rapide + 2 serveurs lents ===");
 
-        double[] muServeurs3 = { 0.0083, 0.00417, 0.00417 };
+        double[] muServeurs3 = { 1/120.0, 0.1/240.0, 1/240.0};
         double[] q3 = { 0.5/3, 0.5/3, 0.5/3 };
 
         ReseauFilesAttente rfa3 = new ReseauFilesAttente(
