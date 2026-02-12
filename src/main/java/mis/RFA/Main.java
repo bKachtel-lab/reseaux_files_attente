f6c4f7bfe9b4ccb90a3f3e9a30bc41f10d0ff869
boukhalfa <boukhalfakcahtel229@gmail.com>
2026-02-12 13:59:54 -0800

    Ajout du test 4

--- src/main/java/mis/RFA/Main.java
+++ src/main/java/mis/RFA/Main.java
@@ -12,17 +12,21 @@ public class Main {
         double muC = 1/10.0;          // Coordinateur : 10 ms
         double p = 0.5;              // Probabilité de sortie
         double dureeSimulation = 100000.0;
-
-        // TEST 1 : 1 serveur rapide
-        System.out.println("= TEST 1 : 1 serveur rapide ==");
-
+        
         double lambda1 = 0.002;
+        double lambda2 = 0.008;
+        double lambda3 = 0.01;
+        
+        // TEST 1 : 1 serveur rapide
+       /* System.out.println("= TEST 1 : 1 serveur rapide ==");
+
+        
 
         double[] muServeurs1 = { 1/120.0 };   // 120 ms
         double[] q1 = { 1.0 };
 
         ReseauFilesAttente rfa1 = new ReseauFilesAttente(
-                lambda1,
+                lambda3,
                 muC,
                 muServeurs1,
                 p,
@@ -40,7 +44,7 @@ public class Main {
         double[] q2 = { 0.5, 0.5 };
 
         ReseauFilesAttente rfa2 = new ReseauFilesAttente(
-                lambda1,
+                lambda3,
                 muC,
                 muServeurs2,
                 p,
@@ -57,7 +61,7 @@ public class Main {
         double[] q3 = { 1.0/3, 1.0/3, 1.0/3 };
 
         ReseauFilesAttente rfa3 = new ReseauFilesAttente(
-                lambda1,
+                lambda3,
                 muC,
                 muServeurs3,
                 p,
@@ -65,7 +69,24 @@ public class Main {
                 dureeSimulation
         );
 
-        rfa3.simuler();
+        rfa3.simuler();*/
+        
+     // TEST 4 : 2 serveurs dont un plus rapide (190 ms)
+        System.out.println("\n== TEST 4 : 2 serveurs (120 ms + 190 ms) =");
+
+        double[] muServeurs4 = { 1/120, 1/190 }; // 120 ms et 190 ms
+        double[] q4 = { 0.25, 0.25 };
+
+        ReseauFilesAttente rfa4 = new ReseauFilesAttente(
+                lambda3,
+                muC,
+                muServeurs4,
+                p,
+                q4,
+                dureeSimulation
+        );
+
+        rfa4.simuler();
 
     }
 }
--- src/main/resources/courbes.gnu
+++ src/main/resources/courbes.gnu
@@ -1,7 +1,13 @@
-set terminal pngcairo size 1000,600
-set output "nt.png"
-set title "N(t) - nombre de clients dans le reseau"
+reset
+set terminal pngcairo size 1200,600
+set output "test_4_nt_n2_0.002.png"
 set xlabel "Temps (ms)"
-set ylabel "N(t)"
+set ylabel "Nombre de requêtes"
+set title "Convergence – Test 4 (n = 2, lambda = 0.002)"
 set grid
-plot "nt_lambda0.002_n1_p0.5_T100000.dat" using 1:2 with steps title "N(t)"
+set xrange [0:50000]
+set xtics 0,5000,100000
+set yrange [0:80]
+set ytics 0,20,40,60,80
+plot 'test_4_nt_lambda0.002_n2_p0.5_T100000.dat' using 1:2 with lines lw 2 lc rgb "blue" title "N(t)"
+set output
